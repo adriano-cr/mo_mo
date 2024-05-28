733c9c1028e764bcecca062d3ddf5b3c6cd66146
adriano-cr <adriano.cotta.ramusino@gmail.com>
2024-05-28 18:46:14 +0200

    Updated page to show actual results, added stylesheet

--- frontend.py
+++ frontend.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from main import fai_le_cose
-import time
+from classes.listing import Listing
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     le_cose = fai_le_cose()
-    return render_template('index.html', content = le_cose)
+    return render_template('index.html', title=le_cose.title, description=le_cose.description, img_url=le_cose.img)
 
 if __name__ == '__main__':
     app.run(debug=True)
