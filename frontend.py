25b9a93de2c9dc07a78747999180fdcdaf0bfa63
adriano-cr <adriano.cotta.ramusino@gmail.com>
2024-06-06 20:12:38 +0200

    Attempts at creating loading screen

--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from main import fai_le_cose
 from classes.listing import Listing
 
@@ -8,9 +8,17 @@ search_title = "seiko automatico"
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/trigger_function', methods=['POST'])
+def trigger_function():
+    launch_search()
+    return render_template('loading.html')
+
+def launch_search():
     le_cose = fai_le_cose(search_title)
     # return render_template('index.html', title=le_cose.title, description=le_cose.description, img_url=le_cose.img)
-    return render_template('index.html', content = le_cose, search_title = search_title)
+    return render_template('results.html', content = le_cose, search_title = search_title)
 
 if __name__ == '__main__':
     app.run(debug=True)
